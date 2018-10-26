2d76dde5ec7b8f1239fd9abf8d78e832a812b1bc
Henadzi Zubaryk <h.zubaryk@godeltech.com>
2018-10-26 15:23:12 +0300

    initial

---
+++ build/precache-manifest.098b45dad58338d884373165cac9508e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "192bd6713b48cf1efb9c",
+    "url": "/static/js/main.192bd671.chunk.js"
+  },
+  {
+    "revision": "508b962ecaa0c00259a7",
+    "url": "/static/js/1.508b962e.chunk.js"
+  },
+  {
+    "revision": "192bd6713b48cf1efb9c",
+    "url": "/static/css/main.2512c9fc.chunk.css"
+  },
+  {
+    "revision": "2277328d11c89834caeab371e960102a",
+    "url": "/index.html"
+  }
+];
---
+++ build/precache-manifest.874eb5aa0fa67a577cd0ba3f38846fed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fabd2378c759e51b7a33",
+    "url": "/static/js/main.fabd2378.chunk.js"
+  },
+  {
+    "revision": "508b962ecaa0c00259a7",
+    "url": "/static/js/1.508b962e.chunk.js"
+  },
+  {
+    "revision": "fabd2378c759e51b7a33",
+    "url": "/static/css/main.b107d046.chunk.css"
+  },
+  {
+    "revision": "a021ddfce0942a08e227ada4e0f2d091",
+    "url": "/index.html"
+  }
+];
